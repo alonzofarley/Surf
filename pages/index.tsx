da25f4f2179869c9cc3f5c8d9760a17739a0fca0
Alonzo Farley <afarleyjr@gmail.com>
2024-09-20 05:06:02 -0400

    added button to homepage to launch hearts app

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,41 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
-
 export default function Index(props: {}) {
-    const router = useRouter();
-    const launchStocks = () => {
-        router.push(`/stocksGame`);
-    }
+  const router = useRouter();
+  const launchStocks = () => {
+    router.push(`/stocksGame`);
+  };
 
-    const launchWaves = () => {
-        router.push(`/waves`);
-    }
+  const launchWaves = () => {
+    router.push(`/waves`);
+  };
 
-    return <div>
-        <h1>Choose Project</h1>
-        <br></br>
-        <Row>
-            <Col>
-                <Button onClick={launchStocks}>Launch Stocks Game</Button>
-            </Col>
-        </Row>
-        <br></br>
-        <Row>
-            <Col>
-                <Button onClick={launchWaves}>Launch Waves</Button>
-            </Col>
-        </Row>
+  const launchHearts = () => {
+    router.push(`/hearts`);
+  };
 
+  return (
+    <div>
+      <h1>Choose Project</h1>
+      <br></br>
+      <Row>
+        <Col>
+          <Button onClick={launchStocks}>Launch Stocks Game</Button>
+        </Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col>
+          <Button onClick={launchWaves}>Launch Waves</Button>
+        </Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col>
+          <Button onClick={launchHearts}>Launch Hearts</Button>
+        </Col>
+      </Row>
     </div>
+  );
 }
